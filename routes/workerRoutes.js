9668f3bcd0565e05cf8ace43937c02f10fae22e2
Capiyo <apiyoosca@gmail.com>
2025-07-16 21:00:43 +0300

    Here

--- routes/workerRoutes.js
+++ routes/workerRoutes.js
@@ -1,18 +1,18 @@
 import express from 'express';
-import Job from '../models/Job.js';
+//import Job from '../models/Job.js';
 
 const router = express.Router();
 
 //import { addCase } from '../controllers/Case/assignWorker/addChats.js';
 //import { getChats } from '../controllers/Case/assignWorker/getChats.js';
 //import { getChats } from '../controllers/Case/WorkerChats/getChats.js';
-import { addChats } from '../controllers/Case/WorkerChats/addChats.js';
-import { getWorkerChats } from '../controllers/Case/workerChats/getWorkerChats.js';
-import{ getJobs} from "../controllers/Job/getJobs.js"
-import { firstChat } from '../controllers/Job/firstChat.js';
+//import { addChats } from '../controllers/Case/WorkerChats/addChats.js';
+//import { getWorkerChats } from '../controllers/Case/workerChats/getWorkerChats.js';
+//import{ getJobs} from "../controllers/Job/getJobs.js"
+//import { firstChat } from '../controllers/Job/firstChat.js';
 
-router.post('/addWorkerChats', addChats); 
-router.get('/getWorkerChats',getWorkerChats); 
+//router.post('/addWorkerChats', addChats); 
+//router.get('/getWorkerChats',getWorkerChats); 
 
 
 

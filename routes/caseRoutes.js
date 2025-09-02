f7f466a313fd7f5db7d0c9f35de891a7d30cb6af
Capiyo <apiyoosca@gmail.com>
2025-09-02 22:05:50 +0300

    u[date

--- routes/caseRoutes.js
+++ routes/caseRoutes.js
@@ -11,9 +11,11 @@ import { getWorkerChats } from '../controllers/Case/workerChats/getWorkerChats.j
 import { getNotification } from '../controllers/Case/getNotification.js';
 //import { getChats } from '../controllers/Case/WorkerChats/getChats.js';
 import { update } from '../controllers/Case/updateCase.js';
+import { addNotification } from '../controllers/Case/addNotification.js';
 
 router.post('/addWorkerChats', addChats); 
 router.post("/addAdmin",addAdmin)
+router.post("/addNotification",addNotification)
 router.get('/getWorkerChats',getWorkerChats ); 
 router.get('/getNotifications/:userId',getNotification); 
 router.put("/current-case/update",update)
